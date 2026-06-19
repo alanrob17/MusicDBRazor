e852564b41557ff8b82cda21bf46cd76fe10d8f1
Alan Robson <alan.robson@gmail.com>
2026-06-19 21:49:25 +1000

    Add Search page

---
+++ MusicDB/Pages/Search/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MusicDB.Pages.Search
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
